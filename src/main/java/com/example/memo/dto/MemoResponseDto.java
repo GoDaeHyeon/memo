4bd74d59e010fc299fe3a3b35620192e5a151b75
Daehyeon <tkd6089@naver.com>
2025-02-03 14:26:36 +0900

    memo

--- src/main/java/com/example/memo/dto/MemoResponseDto.java
+++ src/main/java/com/example/memo/dto/MemoResponseDto.java
@@ -1,4 +1,16 @@
 package com.example.memo.dto;
 
+import com.example.memo.entity.Memo;
+import lombok.Getter;
+
+@Getter
+
 public class MemoResponseDto {
+
+    private Long id;
+    private String title;
+    private String contents;
+
+    public MemoResponseDto(Memo memo) {
+    }
 }
